4c6b3ecffdc2c46dff57d52d4fbc7333ec18bf82
azunaeine <97314182+azunaeine@users.noreply.github.com>
2023-01-06 04:00:35 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+ // When we click on the star button on this homepage
+// navigate to exercise page
+// * Hint Button
+// When we click on the question button
+// navigate to hint page
+// * Answering interaction
+// We click one of the answers
+// and then we click enter
+// If the answer is correct,
+// make the check mark appear
+// else,
+// make the X appear
+// * Progress bar animation
+// When we answer correctly,
+// progress bar goes forward
+// * Back button
+// Navigate to homepage if the back button is on the excercise page
+// When we click on the back button on the hint page
+// navigate to the excercise page
+
